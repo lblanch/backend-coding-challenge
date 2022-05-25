7d1f3cc67c527b06e461c5d2e9f618a9dc5a6876
Laura Blanch <laura.blancho@gmail.com>
2022-05-25 22:38:50 +0300

    Created basic API server and endpoints (without functionality). Implemented logic for reading the .json files. Implemented logic for creating the users.json file out of the actions.js if necessary.

--- main.go
+++ main.go
@@ -1,7 +1,117 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/exp/maps"
+)
+
+type user struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type action struct {
+	ID         int       `json:"id"`
+	Type       string    `json:"type"`
+	UserID     int       `json:"userId"`
+	TargetUser int       `json:"targetUser"`
+	CreatedAt  time.Time `json:"createdAt"`
+}
+
+var actions []action
+var users []user
 
 func main() {
-	fmt.Println("Hello World!")
+
+	// Load data from .json files
+	loadActions()
+	loadUsers()
+
+	//extractUsersFromActions()
+	//saveUsersToFile()
+
+	// Define API's endpoints
+	router := gin.Default()
+	router.GET("/users/:id", getUserByID)
+	router.GET("/users/actions/:id", getActionCountByUserID)
+	router.GET("/actions/next/:type", getNextActionBreakdownByType)
+	router.GET("/users/referralIndex", getUsersReferralIndex)
+
+	// Start the server
+	router.Run("localhost:8080")
+}
+
+func loadActions() {
+	content, err := ioutil.ReadFile("./data/actions.json")
+	if err != nil {
+		log.Fatal("Error when opening actions file: ", err)
+	}
+
+	err = json.Unmarshal(content, &actions)
+	if err != nil {
+		log.Fatal("Error during Unmarshal() for actions: ", err)
+	}
+}
+
+func loadUsers() {
+	content, err := ioutil.ReadFile("./data/users.json")
+	if err != nil {
+		log.Fatal("Error when opening users file: ", err)
+	}
+
+	err = json.Unmarshal(content, &users)
+	if err != nil {
+		log.Fatal("Error during Unmarshal() for users: ", err)
+	}
+}
+
+func extractUsersFromActions() {
+	userMap := make(map[string]user)
+
+	for _, action := range actions {
+		userMap[strconv.Itoa(action.UserID)] = user{
+			ID:        action.UserID,
+			Name:      strings.Join([]string{"User", strconv.Itoa(action.UserID)}, ""),
+			CreatedAt: action.CreatedAt,
+		}
+	}
+
+	users = maps.Values(userMap)
+}
+
+func saveUsersToFile() {
+	content, err := json.Marshal(users)
+	if err != nil {
+		log.Fatal("Error during Marshal() for users: ", err)
+	}
+
+	err = ioutil.WriteFile("./data/users.json", content, 0644)
+	if err != nil {
+		log.Fatal("Error when saving users file: ", err)
+	}
+}
+
+func getUserByID(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "TBD"})
+}
+
+func getActionCountByUserID(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "TBD"})
+}
+
+func getNextActionBreakdownByType(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "TBD"})
+}
+
+func getUsersReferralIndex(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "TBD"})
 }
