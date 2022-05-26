0adf5ca6725e4929a221db7e698a8f751f310925
Laura Blanch <laura.blancho@gmail.com>
2022-05-26 11:15:22 +0300

    Implemented endpoint /users/referralIndex according to Q4. MINOR: Replaced all c.IndentedJSON for c.JSON as is more efficient

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	loadActions()
 	loadUsers()
 
-	// In case of non-existing users.json: comment above line and uncomment both below lines
+	// In case of non-existing users.json file: comment above line and uncomment both below lines
 	//extractUsersFromActions()
 	//saveUsersToFile()
 
@@ -94,6 +94,7 @@ func extractUsersFromActions() {
 		}
 	}
 
+	// Experimental function, not to be used in prod
 	users = maps.Values(userMap)
 }
 
@@ -112,24 +113,24 @@ func saveUsersToFile() {
 func getUserByID(c *gin.Context) {
 	receivedID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
 		return
 	}
 
 	for _, user := range users {
 		if user.ID == receivedID {
-			c.IndentedJSON(http.StatusOK, user)
+			c.JSON(http.StatusOK, user)
 			return
 		}
 	}
 
-	c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+	c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
 }
 
 func getActionCountByUserID(c *gin.Context) {
 	receivedID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
 		return
 	}
 
@@ -141,7 +142,7 @@ func getActionCountByUserID(c *gin.Context) {
 		}
 	}
 
-	c.IndentedJSON(http.StatusOK, gin.H{"count": count})
+	c.JSON(http.StatusOK, gin.H{"count": count})
 }
 
 func getNextActionBreakdownByType(c *gin.Context) {
@@ -176,9 +177,42 @@ func getNextActionBreakdownByType(c *gin.Context) {
 		nextActionMap[index] = actionCount / float64(totalNextActions)
 	}
 
-	c.IndentedJSON(http.StatusOK, nextActionMap)
+	c.JSON(http.StatusOK, nextActionMap)
 }
 
 func getUsersReferralIndex(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "TBD"})
+	type userReferralInfo struct {
+		userID  int
+		referer int // ID of user who referred this user
+	}
+
+	// Map to keep track of user referrals
+	userReferralIndexMap := make(map[int]int)
+
+	// Slice to keep track of the order in which users are referred, and their referrers
+	var referralsOrder []userReferralInfo
+
+	// Populate the map with each user's direct referrals
+	for _, action := range actions {
+		if action.Type == "REFER_USER" {
+			// Increase the amount of users referred by action.UserID
+			userReferralIndexMap[action.UserID] = userReferralIndexMap[action.UserID] + 1
+
+			// Keep track of the order in which users are referred and
+			// define action.UserID as the referrer for action.TargetUser
+			referralsOrder = append(referralsOrder, userReferralInfo{action.TargetUser, action.UserID})
+		}
+	}
+
+	// Add indirect referrals to calculate final referral index
+	// Traverse the referralsOrder slice in reverse, skipping the last position
+	// as it would have referrals = 0
+	for i := len(referralsOrder) - 2; i >= 0; i-- {
+		targetInfo := referralsOrder[i]
+
+		// Update referer's referral index to include referred's referrals (indirect referrals)
+		userReferralIndexMap[targetInfo.referer] = userReferralIndexMap[targetInfo.referer] + userReferralIndexMap[targetInfo.userID]
+	}
+
+	c.JSON(http.StatusOK, userReferralIndexMap)
 }
